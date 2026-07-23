2779ba51f818baaf0d3f84dba7befab0e6e6a23c
Joshua Sentana <joshua.sentana@gmail.com>
2026-07-23 13:46:41 +0700

    Added basic character controller

--- gjmt/Scripts/Player.gd
+++ gjmt/Scripts/Player.gd
@@ -1,97 +1,134 @@
 extends CharacterBody3D
-
 class_name Player
 
-@onready var input_gatherer = $Input as InputGatherer
-@onready var model = $Model as PlayerModel
-@onready var visuals = $Visuals as PlayerVisuals
-@onready var camera_mount: Node3D = $CameraMount
+@export_category("Movement")
 
-func _ready():
-	visuals.accept_model(model)
+@export var walk_speed: float = 5.0
+@export var sprint_speed: float = 8.0
+
+@export var ground_acceleration: float = 25.0
+@export var ground_deceleration: float = 30.0
+
+@export var air_acceleration: float = 8.0
+@export var air_deceleration: float = 3.0
+
+@export var jump_velocity: float = 4.8
+
+@export_category("Camera")
+
+@export var mouse_sensitivity: float = 0.004
+
+@export var min_look_angle: float = -40.0
+@export var max_look_angle: float = 60.0
+
+@export_category("Head Bob")
+
+@export var bob_frequency: float = 2.4
+@export var bob_amplitude: float = 0.08
+
+var bob_time: float = 0.0
+
+@export_category("FOV")
+
+@export var base_fov: float = 75.0
+@export var fov_change: float = 1.5
+@export var fov_smoothing: float = 8.0
+
+@onready var camera_mount: Node3D = $CameraMount
+@onready var camera: Camera3D = $CameraMount/PlayerCam
+
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+# LIFECYCLE
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		_handle_camera_look(event)
 
 func _physics_process(delta: float) -> void:
-	var input = input_gatherer.gather_input()
-	model.update(input, delta)
-	
-	input.queue_free()
+	_apply_gravity(delta)
+	_handle_jump()
+	_handle_movement(delta)
+	_update_head_bob(delta)
+	_update_fov(delta)
+	move_and_slide()
 
-#var speed
-#const WALK_SPEED = 5.0
-#const SPRINT_SPEED = 8.0
-#const JUMP_VELOCITY = 4.8
-#const SENSITIVITY = 0.004
-#
-##bob variables
-#const BOB_FREQ = 2.4
-#const BOB_AMP = 0.08
-#var t_bob = 0.0
-#
-##fov variables
-#const BASE_FOV = 75.0
-#const FOV_CHANGE = 1.5
-#
-## Get the gravity from the project settings to be synced with RigidBody nodes.
-#var gravity = 9.8
-#
-#@onready var camera_mount = $CameraMount
-#@onready var camera = $CameraMount/PlayerCam
-#
-#
-#func _ready():
-	#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-#
-#
-#func _unhandled_input(event):
-	#if event is InputEventMouseMotion:
-		#camera_mount.rotate_y(-event.relative.x * SENSITIVITY)
-		#camera.rotate_x(-event.relative.y * SENSITIVITY)
-		#camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
-#
-#
-#func _physics_process(delta):
-	## Add the gravity.
-	#if not is_on_floor():
-		#velocity.y -= gravity * delta
-#
-	## Handle Jump.
-	#if Input.is_action_just_pressed("jump") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-	#
-	## Handle Sprint.
-	#if Input.is_action_pressed("sprint"):
-		#speed = SPRINT_SPEED
-	#else:
-		#speed = WALK_SPEED
-#
-	## Get the input direction and handle the movement/deceleration.
-	#var input_dir = Input.get_vector("left", "right", "forward", "backward")
-	#var direction = (camera_mount.transform.basis * transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	#if is_on_floor():
-		#if direction:
-			#velocity.x = direction.x * speed
-			#velocity.z = direction.z * speed
-		#else:
-			#velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0)
-			#velocity.z = lerp(velocity.z, direction.z * speed, delta * 7.0)
-	#else:
-		#velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
-		#velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
-	#
-	## Head bob
-	#t_bob += delta * velocity.length() * float(is_on_floor())
-	#camera.transform.origin = _camera_mountbob(t_bob)
-	#
-	## FOV
-	#var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED * 2)
-	#var target_fov = BASE_FOV + FOV_CHANGE * velocity_clamped
-	#camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
-	#
-	#move_and_slide()
-#
-#
-#func _camera_mountbob(time) -> Vector3:
-	#var pos = Vector3.ZERO
-	#pos.y = sin(time * BOB_FREQ) * BOB_AMP
-	#pos.x = cos(time * BOB_FREQ / 2) * BOB_AMP
-	#return pos
+
+
+# CAMERA LOOK
+func _handle_camera_look(event: InputEventMouseMotion) -> void:
+	# Rotate the player horizontally.
+	rotate_y(-event.relative.x * mouse_sensitivity)
+
+	# Rotate the camera vertically.
+	camera.rotation.x -= event.relative.y * mouse_sensitivity
+	camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(min_look_angle), deg_to_rad(max_look_angle))
+
+
+# GRAVITY AND JUMPING
+func _apply_gravity(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+func _handle_jump() -> void:
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+
+# MOVEMENT
+func _handle_movement(delta: float) -> void:
+	var input_direction := Input.get_vector(
+		"left",
+		"right",
+		"forward",
+		"backward"
+	)
+
+	var movement_direction := _get_movement_direction(input_direction)
+	var current_speed := sprint_speed if Input.is_action_pressed("sprint") else walk_speed
+	if is_on_floor():
+		_move_on_ground(movement_direction, current_speed, delta)
+	else:
+		_move_in_air(movement_direction, current_speed, delta)
+
+
+func _get_movement_direction(input_direction: Vector2) -> Vector3:
+	var direction := Vector3(input_direction.x, 0.0, input_direction.y)
+	# Movement follows the player's horizontal rotation.
+	direction = global_transform.basis * direction
+	return direction.normalized()
+
+func _move_on_ground(direction: Vector3, speed: float, delta: float) -> void:
+	var target_velocity = direction * speed
+	if direction:
+		velocity.x = move_toward(velocity.x, target_velocity.x, ground_acceleration * delta)
+		velocity.z = move_toward(velocity.z, target_velocity.z, ground_acceleration * delta)
+	else:
+		velocity.x = move_toward(velocity.x, 0.0, ground_deceleration * delta)
+		velocity.z = move_toward(velocity.z, 0.0, ground_deceleration * delta)
+
+func _move_in_air(direction: Vector3, speed: float, delta: float) -> void:
+	var target_velocity := direction * speed
+	velocity.x = move_toward(velocity.x, target_velocity.x, air_acceleration * delta)
+	velocity.z = move_toward(velocity.z, target_velocity.z, air_acceleration * delta)
+
+# HEAD BOB
+func _update_head_bob(delta: float) -> void:
+	var horizontal_speed := Vector2(velocity.x, velocity.z).length()
+	if is_on_floor() and horizontal_speed > 0.1:
+		bob_time += delta * horizontal_speed
+		camera.position = _get_head_bob_position(bob_time)
+	else:
+		camera.position = camera.position.lerp(Vector3.ZERO, delta * 10.0)
+
+func _get_head_bob_position(time: float) -> Vector3:
+	var bob_position := Vector3.ZERO
+	bob_position.y = sin(time * bob_frequency) * bob_amplitude
+	bob_position.x = cos(time * bob_frequency * 0.5) * bob_amplitude
+	return bob_position
+
+func _update_fov(delta: float) -> void:
+	var horizontal_speed := Vector2(velocity.x, velocity.z).length()
+	var target_fov := base_fov + (fov_change * horizontal_speed)
+	camera.fov = lerp(camera.fov, target_fov, fov_smoothing * delta)
