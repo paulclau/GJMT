807ec4cf064bdd721012c0d4647640f4ea8102cf
saksey1 <Yashsaksena18@gmail.com>
2026-07-27 01:08:49 +1000

    hub world functional

--- Memento/Scripts/PillowSpawning.gd
+++ Memento/Scripts/PillowSpawning.gd
@@ -2,48 +2,62 @@
 extends Node
 
 var pillow_scene = preload("res://Scenes/Pillow/Pillow.tscn")
-var player_ref:Player;
-var _player_position:Vector3;
-var _current_velocity:Vector3;
-var pillows = [];
+var player_ref: Player
+var _player_position: Vector3
+var _current_velocity: Vector3
+var is_spawning: bool = false
+
+@export var SpawnInterval: float = 1.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if(!pillow_scene):
+	if not pillow_scene:
 		print("Error: No Pillow scene Set")
-		return;
+		return
 	
-	player_ref = get_tree().get_first_node_in_group("player");
+	player_ref = get_tree().get_first_node_in_group("player")
 	
-	if(!player_ref):
+	if not player_ref:
 		print("Error: NO PLAYER WTH")
-		return;
+		return
 	
-	_player_position = player_ref.global_position;
-	_current_velocity = player_ref.velocity;
+	_player_position = player_ref.global_position
+	_current_velocity = player_ref.velocity
 	
-	create_pillow();
+	create_pillow()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if(!player_ref):
-		return;
-	_player_position = player_ref.global_position;
-	_current_velocity = player_ref.velocity;
-	if(_current_velocity.length() != 0):
-		create_pillow();
+func _process(_delta: float) -> void:
+	if not player_ref:
+		return
+	_player_position = player_ref.global_position
+	_current_velocity = player_ref.velocity
 	
-func create_pillow():
-	var player_bounds:AABB = player_ref.get_player_bounds();
-	player_bounds = player_bounds.abs();
-	var player_halfheight = player_bounds.get_longest_axis_size() / 2;
+	if _current_velocity.length() > 0 and not is_spawning:
+		create_pillow()
 	
+func create_pillow() -> void:
+	is_spawning = true
+	
+	await get_tree().create_timer(SpawnInterval).timeout
+	
+	if not is_instance_valid(player_ref):
+		is_spawning = false
+		return
+
+	var player_bounds: AABB = player_ref.get_player_bounds()
+	player_bounds = player_bounds.abs()
+	var player_halfheight = player_bounds.get_longest_axis_size() / 2
+	
+	var current_pos = player_ref.global_position
 	var PillowPosition = Vector3(
-		_player_position.x,
-		_player_position.y - player_halfheight - 0.1, # variance of 0.1m
-		_player_position.z)
-	# check if position is valid
+		current_pos.x,
+		current_pos.y - player_halfheight - 0.25,
+		current_pos.z
+	)
 	
-	var new_pillow = pillow_scene.instantiate(PackedScene.GEN_EDIT_STATE_DISABLED) as Node3D;
-	get_parent().add_child.call_deferred(new_pillow);
-	new_pillow.global_position = PillowPosition;
+	var new_pillow = pillow_scene.instantiate(PackedScene.GEN_EDIT_STATE_DISABLED) as Node3D
+	get_parent().add_child.call_deferred(new_pillow)
+	new_pillow.global_position = PillowPosition
+	
+	is_spawning = false # Unlock the function
